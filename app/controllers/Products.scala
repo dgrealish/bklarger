eac1d16846d195768c16405111529ee052169e1d
Darragh Grealish <darragh.grealish@gmail.com>
2014-07-01 12:03:29 +0200

    Adding product list and barcode generation

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -5,8 +5,18 @@ import models.Product
 
 object Products extends Controller {
 	def list = Action { implicit request =>
-	val products = Product.findAll
+	
+		val products = Product.findAll
 
-	Ok(views.html.products.list(products))
+		Ok(views.html.products.list(products))
+	}
+
+	def show(ean: Long) = Action { implicit request => 
+
+		Product.findByEan(ean).map { product =>
+
+			Ok(views.html.products.details(product))
+			
+		}.getOrElse(NotFound)
 	}
 }
--- conf/messages.es
+++ conf/messages.es
@@ -1,1 +1,5 @@
 application.name = Catálogue de productos
+ean = EAN
+name = Nombre
+description = Descripción
+products.details = Producto: {0}
